b9fc948e46e87844f4f3ac0fdc887053367b96fe
Marco Vignati <marcovignatidev@gmail.com>
2024-03-06 03:52:32 -0400

    update home with links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,18 @@
+import { Button } from "@nextui-org/react";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main>
+    <main className="w-fit m-auto mt-8 flex gap-2">
+      <Link href="/produtos">
+        <Button color="primary">Produtos</Button>
+      </Link>
+      <Link href="/fornecedores">
+        <Button color="primary">Fornecedores</Button>
+      </Link>
+      <Link href="/cadastro">
+        <Button color="primary">Cadastro</Button>
+      </Link>
     </main>
   );
 }
